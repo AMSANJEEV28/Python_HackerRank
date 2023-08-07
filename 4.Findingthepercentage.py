a3f1ee98a373f7e27b8f6382f219814cb13c9695
A M SANJEEV <amsanjeev28@gmail.com>
2023-08-08 00:17:43 +0530

    Basic Data Types

---
+++ 4.Findingthepercentage.py
@@ -0,0 +1,17 @@
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    
+    
+    if query_name in student_marks:
+        marks=student_marks[query_name]
+        avg_marks = sum(marks)/len(marks)
+        print("{:.2f}".format(avg_marks))
+    else:
+        print("Not Found")
