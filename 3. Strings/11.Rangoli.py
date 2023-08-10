e4e23b6a05037cd383056ef4b6bc551d882422de
A M SANJEEV <amsanjeev28@gmail.com>
2023-08-10 19:21:59 +0530

    Strings

---
+++ 3. Strings/11.Rangoli.py
@@ -0,0 +1,15 @@
+def print_rangoli(size):
+    import string
+
+    alphabet = string.ascii_lowercase
+    lines = []
+
+    for i in range(size):
+        line = '-'.join(alphabet[size - 1:i:-1] + alphabet[i:size])
+        lines.append(line.center(4 * size - 3, '-'))
+
+    print('\n'.join(lines[:0:-1] + lines))
+
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
